dd0457a4858bc13b3867e19cfd618cbef7a7a374
Iñaki Azkue <inazkue@gmail.com>
2015-12-03 19:32:13 +0100

    Adding for multi-vm

---
+++ scripts/centos-common.sh
@@ -0,0 +1,6 @@
+
+#update Centos with any patches
+yum update -y --exclude=kernel
+
+#Tools
+yum install -y nano git unzip screen nc telent
---
+++ scripts/centos-web.sh
@@ -0,0 +1,20 @@
+#Apache
+yum install -y httpd httpd-devel httpd-tools
+chkconfig --add httpd
+chkconfig httpd on 
+service httpd stop
+
+rm -rf /var/www/html
+ln -s /vagrant /var/www/html
+
+service httpd start
+
+#php
+yum install -y php php-cli php-devel php-mysql
+
+
+#Download Starter Content
+cd /vagrant
+sudo -u vagrant wget -q https://raw.githubusercontent.com/inazkue/vagrant/master/files/index.html
+sudo -u vagrant wget -q https://raw.githubusercontent.com/inazkue/vagrant/master/files/info.php
+service httpd restart
